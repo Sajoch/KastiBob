b135ba1073d2267f796af690bb7e069aa5c331ea
Sajoch <pojtrot@gmail.com>
2016-10-15 13:55:20 +0200

    add bin2c

---
+++ bin2c/bin2c.c
@@ -0,0 +1,24 @@
+//https://gist.github.com/albertz/1551304
+//Author albertz
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	if(argc != 2){
+			printf("using: <file>\n");
+			return 1;
+	}
+	char* fn = argv[1];
+	FILE* f = fopen(fn, "r");
+	printf("unsigned char a[] = {");
+	unsigned long n = 0;
+	while(!feof(f)) {
+		unsigned char c;
+		if(fread(&c, 1, 1, f) == 0) break;
+		printf("0x%.2X,", (int)c);
+		++n;
+	}
+	fclose(f);
+	printf("};\n");
+	printf("unsigned long al=%d;\n",n);
+	return 0;
+}
