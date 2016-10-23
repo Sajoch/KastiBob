25103a90df1fc3ce0a32ce6df526ea7da56b6cab
Sajoch <pojtrot@gmail.com>
2016-10-23 20:23:33 +0200

    fix

--- running_inject/src/remoteCode.c
+++ running_inject/src/remoteCode.c
@@ -5,13 +5,9 @@
 
 //static void* addr_MessageBox = (void*)0xDEADBEAF;
 unsigned int addr_baseKernel32 = 0;
-void* addr_GetProcAddress = 0;
-void* addr_LoadLibrary = 0;
-void* addr_GetModuleHandle = 0;
-
-void print(void* addr){
-	return 0;
-}
+unsigned int addr_GetProcAddress = 0;
+unsigned int addr_LoadLibrary = 0;
+unsigned int addr_GetModuleHandle = 0;
 
 int Mstrcmp(char* a, char* b){
 	while(*a!=0 || *b!=0){
@@ -25,7 +21,6 @@ int Mstrcmp(char* a, char* b){
 }
 
 int loadFunction(){
-	char* cmpstr = 0;
 	if(addr_baseKernel32 == 0){
 		return 1;
 	}
@@ -35,32 +30,15 @@ int loadFunction(){
 		return 2;
 	}
 	IMAGE_EXPORT_DIRECTORY* ed = (void*)(addr_baseKernel32 + nt_headers->OptionalHeader.DataDirectory[0].VirtualAddress);
-																/*
-	unsigned int addr_section_export = 0;
 
-	unsigned int check_section = (unsigned int)nt_headers + sizeof(IMAGE_NT_HEADERS);
-	for(unsigned int s=1; s < nt_headers->FileHeader.NumberOfSections; s++){
-		IMAGE_SECTION_HEADER* sec = check_section + sizeof(IMAGE_SECTION_HEADER) * (s-1);
-		unsigned int start = addr_baseKernel32 + sec->VirtualAddress;
-		unsigned int stop = addr_baseKernel32 + sec->VirtualAddress + sec->Misc.VirtualSize;
-		//print(start);
-		if (start <= addr_eq && stop > addr_eq) {
-			addr_section_export = start;
-		}
-	}
-	if (addr_section_export == 0) {
-		return 3;
-	}
-	*/
 	unsigned int names_ed = ed->AddressOfNames + addr_baseKernel32;
 	unsigned int addresses_ed = ed->AddressOfFunctions + addr_baseKernel32;
 	unsigned int ordinal_ed = ed->AddressOfNameOrdinals + addr_baseKernel32;
 	for (unsigned int i=0;i<ed->NumberOfFunctions;i++){
-		//cout<<(names_ed[i])<<" = "<<(names_ed[i])<<endl;
-		unsigned short* ordinal_rva = ordinal_ed+i*2;
-		unsigned int* func = addresses_ed+(*ordinal_rva)*4;
-		unsigned int* name_rva = names_ed+i*4;
-		const char* name = addr_baseKernel32+*name_rva;
+		unsigned short* ordinal_rva = (unsigned short*)(ordinal_ed+i*2);
+		unsigned int* func = (unsigned int*)(addresses_ed+(*ordinal_rva)*4);
+		unsigned int* name_rva = (unsigned int*)(names_ed+i*4);
+		char* name = (char*)(addr_baseKernel32+*name_rva);
 		if (Mstrcmp(name, "GetProcAddress")){
 			addr_GetProcAddress = addr_baseKernel32 + *func;
 		}else if (Mstrcmp(name, "LoadLibraryA")){
@@ -69,18 +47,16 @@ int loadFunction(){
 			addr_GetModuleHandle = addr_baseKernel32 + *func;
 		}
 	}
-	//*/
 	return 0;
 }
 
 unsigned int RemoteThread(void* arg){
 	loadFunction();
-	FARPROC(*_GetProcAddress)(HMODULE,LPCSTR) = addr_GetProcAddress;
-	HMODULE(*_LoadLibrary)(LPCTSTR) = addr_LoadLibrary;
-	HMODULE(*_GetModuleHandle)(LPCTSTR) = addr_GetModuleHandle;
+	FARPROC(*_GetProcAddress)(HMODULE,LPCSTR) = (void*)addr_GetProcAddress;
+	HMODULE(*_LoadLibrary)(LPCTSTR) = (void*)addr_LoadLibrary;
+	HMODULE(*_GetModuleHandle)(LPCTSTR) = (void*)addr_GetModuleHandle;
 	int(*_MessageBox)(HWND, LPCTSTR, LPCTSTR, UINT);
 	HMODULE hDllUser32 = _LoadLibrary("User32.dll");
 	_MessageBox=(void*)_GetProcAddress(hDllUser32,"MessageBoxA");
-	_MessageBox(0,"Halo","Hello World!", MB_OK);
 	return 0;
 }
