28dbd73cdfcead902ea80b1446de4384836f5520
Sajoch <pojtrot@gmail.com>
2016-11-25 23:02:10 +0100

    xtea encrypt hook

--- KastiDll/src/main.cpp
+++ KastiDll/src/main.cpp
@@ -20,7 +20,7 @@ bool change_memory(void* dest, void* src, size_t size){
 }
 
 int(*WS_send)(int, char*, int, int);
-
+int*(*xtea_encrypt_ret)(int*, int*);
 
 void showBuffer(char* buf, int size){
 	string buffer(buf, size);
@@ -32,12 +32,22 @@ void showBuffer(char* buf, int size){
 		}
 	}
 }
+void showHex(char* buf, size_t len){
+	for(size_t i=0;i<len;i++)
+		cout<<(void*)(uint8_t)buf[i]<<" ";
+	cout<<endl;
+}
 
 
 int __stdcall sendWrapper(int sock, char* buf, int size, int flag){
 	int ret = WS_send(sock, buf, size, flag);
 	cout<<"call "<<ret<<" = send("<<sock<<","<<size<<","<<flag<<");"<<endl;
 	showBuffer(buf, size);
+}
+
+int* xtea_encryptWrapper(int* buf, int* keys){
+	showHex((char*)buf, 8);
+	return xtea_encrypt_ret(buf, keys);
 }
 
 
@@ -52,8 +62,19 @@ int ep_main() {
 	char* tibia_info = (char*)(baseAddress+0x2421C8);
 	char* rsa_n_num = (char*)(baseAddress+0x1B2610);
 	
-	//TODO find xtea keys
+	//hook xtea_encrypt
+	{
+		xtea_encrypt_ret = (int*(*)(int*, int*))(baseAddress+0x14ff80);
+		char* xtea_encrypt_func = (char*)(baseAddress+0x15021d);
+		cout<<"xtea_encrypt "<<(void*)xtea_encrypt_func<<endl;
+		unsigned char hook[] = {0xE8, 0x5E, 0xFD, 0xFF, 0xFF};
+		hook[0] = 0xE8;
+		uint32_t xtea_encrypt_add = (uint32_t)&xtea_encryptWrapper - (uint32_t)xtea_encrypt_func - 5;
+		memcpy(&hook[1], (void*)&xtea_encrypt_add, 4);
+		change_memory(&xtea_encrypt_func[0],(void*)hook,5);
+	}
 	
+	//hook send
 	HMODULE ws2 = GetModuleHandle("ws2_32.dll");
 	WS_send = (int(*)(int, char*, int, int))GetProcAddress(ws2, "send");
 	if(WS_send != 0){
